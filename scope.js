4f7afef4013d282e637f9fd0a4be5b89346bbbf2
selam <selam0315@gmail.com>
2018-12-29 15:57:47 -0600

    Mod Squad scope

--- scope.js
+++ scope.js
@@ -11,3 +11,20 @@ const conjunction = function (firstWord, secondWord) {
     console.log(conjoinedWord)
 }
 conjunction("Master", "Card")
+
+//Mod Squad
+{
+    const ModSquad = {
+        "members": ["Pete Cochran", "Linc Hayes", "Julie Barnes", "Capt. Adam Greer", "Chief Barney Metcalf"],
+        "series": {
+            "start": "1968",
+            "end": "1973"
+        }
+    }
+    let HTMLRepresentation = `<h1>The Mod Squad</h1>`
+
+    ModSquad.members.forEach( member => { 
+        HTMLRepresentation += `<div>${member}</div>`
+    })
+    document.querySelector(".show-info").innerHTML = HTMLRepresentation
+}
